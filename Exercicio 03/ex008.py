c41cdda84571892ce4914a9e250fff01e66b4c7f
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-05-11 14:22:22 -0300

    fim do mundo 01

---
+++ Exercicio 03/ex008.py
@@ -0,0 +1,10 @@
+#Trabalhando com sistema de cores
+#-[inicio][sytele][text][fundo][final]-#
+print('\033[1;30;40mOlá,mundo!\033[m')
+print('\033[1;31;41mOlá,mundo!\033[m')
+print('\033[1;32;42mOlá,mundo!\033[m')
+print('\033[1;33;43mOlá,mundo!\033[m')
+print('\033[1;34;44mOlá,mundo!\033[m')
+print('\033[1;35;45mOlá,mundo!\033[m')
+print('\033[1;36;46mOlá,mundo!\033[m')
+print('\033[1;34;44mOlá,mundo!\033[m')
